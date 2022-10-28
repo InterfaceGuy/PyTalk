8a41787d7ecf48c814c11069890390d91588c915
Interface Guy <david.rug98@icloud.com>
2022-10-28 18:41:16 +0200

    implemented reload at runtime using importlib!

--- imports.py
+++ imports.py
@@ -3,8 +3,69 @@ this file is used to simplify importing the necessary modules for video scriptin
 this way we just have to include "from pydeation.imports import *" in our scripts
 """
 
+
+# rreload function not used right now but saved for future reference
+from types import ModuleType
+
+try:
+    from importlib import reload  # Python 3.4+
+except ImportError:
+    # Needed for Python 3.0-3.3; harmless in Python 2.7 where imp.reload is just an
+    # alias for the builtin reload.
+    from imp import reload
+
+def rreload(module):
+    """Recursively reload modules."""
+    reload(module)
+    for attribute_name in dir(module):
+        attribute = getattr(module, attribute_name)
+        if type(attribute) is ModuleType:
+            rreload(attribute)
+
+
+
+# first we reload the sublibraries to update the changes
+import sys
+import importlib
+import pydeation.scene
+import pydeation.objects.helper_objects
+import pydeation.objects.camera_objects
+import pydeation.objects.custom_objects
+import pydeation.objects.effect_objects
+import pydeation.objects.line_objects
+import pydeation.objects.solid_objects
+import pydeation.objects.sketch_objects
+import pydeation.constants
+import pydeation.xpresso.xpresso
+import pydeation.xpresso.xpressions
+import pydeation.xpresso.userdata
+import pydeation.animation.abstract_animators
+
+pydeation_path = "/Users/davidrug/Library/Preferences/Maxon/Maxon Cinema 4D R26_8986B2D7/python39/libs/pydeation"
+
+if pydeation_path not in sys.path:
+    print("add path")
+    sys.path.insert(0, pydeation_path)
+
+reload(pydeation.scene)
+reload(pydeation.objects.helper_objects)
+reload(pydeation.objects.camera_objects)
+reload(pydeation.objects.custom_objects)
+reload(pydeation.objects.effect_objects)
+reload(pydeation.objects.line_objects)
+reload(pydeation.objects.solid_objects)
+reload(pydeation.objects.sketch_objects)
+reload(pydeation.constants)
+reload(pydeation.xpresso.xpresso)
+reload(pydeation.xpresso.xpressions)
+reload(pydeation.xpresso.userdata)
+reload(pydeation.animation.abstract_animators)
+
+
+# then we import the objects from the sublibraries
 from pydeation.scene import *
 from pydeation.objects.helper_objects import *
+from pydeation.objects.camera_objects import *
 from pydeation.objects.custom_objects import *
 from pydeation.objects.effect_objects import *
 from pydeation.objects.line_objects import *
